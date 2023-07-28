ff9647fc3cade97460476f6edd7c784b9f3d9e4e
okaradev <okaracyber@gmail.com>
2023-07-28 12:16:11 +0300

    firstcommit

--- app/src/main/java/com/krypt/bluecoin/LoginActivity.java
+++ app/src/main/java/com/krypt/bluecoin/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -89,12 +90,13 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 UserModel post = dataSnapshot.getValue(UserModel.class);
+                Log.e("yy",post.getEmail());
                 System.out.println(post);
                 if(post.getUsername()==usernm && post.getPass()==pass){
                     Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(LoginActivity.this,MainActivity.class));
                     finish();
-                }else Toast.makeText(LoginActivity.this, "Incorrect credientials", Toast.LENGTH_SHORT).show();
+                }else Toast.makeText(LoginActivity.this, "Incorrect credientials"+post.getUsername(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
